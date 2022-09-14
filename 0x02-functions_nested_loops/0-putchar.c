b30d972e09bdbd996e44a52a6ff0673305a14d8e
henok <teklemariammossie1@gmail.com>
2022-09-14 00:24:02 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,14 +3,14 @@
 /**
  * main - print putchar
  * Return: always 0
- */
+ * */
 
 
 
 int main(void)
 
 {
-
+	
 _putchar('_');
 
 _putchar('p');
@@ -22,7 +22,7 @@ _putchar('t');
 _putchar('c');
 
 _putchar('h');
-
+	
 _putchar('a');
 
 _putchar('r');
@@ -31,4 +31,3 @@ _putchar('\n');
 
 return (0);
 }
-
