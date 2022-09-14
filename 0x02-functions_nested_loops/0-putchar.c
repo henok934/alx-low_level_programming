bd73c509d25404bb57565f512d6d0454369c3157
henok <teklemariammossie1@gmail.com>
2022-09-13 18:54:44 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,21 +5,29 @@
  * Return: Always 0 (Success)
  */
 
+
+
 int main(void)
 	
 {
-        _putchar('t');
-        _putchar('e');
-        _putchar('k');
-	_putchar('l');
-	_putchar('e');
-	_putchar('m');
-  	_putchar('a');
-	_putchar('r');	
-        _putchar('i');
-        _putchar('a');
-        _putchar('m');
-        _putchar('\n');
-	
-	return (0);
+
+_putchar('_');
+
+_putchar('p');
+
+_putchar('u');
+
+_putchar('t');
+
+_putchar('c');
+
+_putchar('h');
+
+_putchar('a');
+
+_putchar('r');	
+
+_putchar('/n');
+
+return (0);
 }	
