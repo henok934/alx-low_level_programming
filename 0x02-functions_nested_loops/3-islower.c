580b3482e93bc5e295fcc97123b20d1d962a850d
henok <teklemariammossie1@gmail.com>
2022-09-14 20:12:28 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -9,13 +9,9 @@
 int _islower(int c)
 
 {
-
-	if (c >= 'a' && c <= 'z')
-	       
-	       	return (1);
-        	
+	if (c >= 'a' && c <= 'z') 
+	       	return (1);	
 	else
-
  	       
 	       return (0);
 }
