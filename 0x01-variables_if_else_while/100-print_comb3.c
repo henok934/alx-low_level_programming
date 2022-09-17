17aac44e6d33ea76eee7ca483f0a538362aadce9
henok <teklemariammossie1@gmail.com>
2022-09-17 09:14:04 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - prints two digit combinations
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int firet, second;
+
+	for (first = 0; first <= 9; first++)
+	{
+		for (second = 0; second <= 9; second++)
+		{
+			if (first != second)
+			{
+				if (first == 0 || second > first)
+				{
+					putchar(first + '0');
+					putchar(second + '0');
+					if (!((first == 8) && (second == 9))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
