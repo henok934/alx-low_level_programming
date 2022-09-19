4dde7a5581e856c02a8a86b066516a5d6f400a48
henok <teklemariammossie1@gmail.com>
2022-09-19 13:19:14 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _puts - prints string that it receives
+* @str: string pointer passed to function _puts
+*/
+void _puts(char *str)
+{
+
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		++str;
+	}
+	
+	_putchar('\n');
+}
