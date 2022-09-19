6100c86ad8fc1423edb934afcfa0531ed1eda75e
henok <teklemariammossie1@gmail.com>
2022-09-19 14:06:01 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,16 +9,19 @@ void print_rev(char *s)
 {
 	int length = 0;
 	int index;
-	while (*(s + length) != '\0')
 
+	while (*(s + length) != '\0')
 	{
 		length++;
 	}
+
 	index = length - 1;
 
 	while (index >= 0)
-	{											_putchar(*(s + index));index--;	
+	{
+		_putchar(*(s + index));index--;	
 		index--;	
 	}
 	_putchar('\n');
+
 }
