1746ef635d993ad748c807c12fc4fe4b1bed9869
henok <teklemariammossie1@gmail.com>
2022-09-27 07:58:16 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,19 +11,22 @@
 
 char  *_strstr(char *haystack, char *needle)
 {
-char *str1, *str2; /*Declaring variables*/
-while (*haystack != '\0')
-{
-str1 = haystack; /*values*/
-str2 = needle;/*Star WHILE*/
-while (*haystack != '\0' && *str2 != '\0' && *haystack == *str2
-{
-haystack++;
-str2++;
-}
-if (*str2 == '\0')
-return (str1);
-haystack = str1 + 1;
-}
-return ('\0');
+	char *str1, *str2; /*Declaring variables*/
+
+	while (*haystack != '\0')
+	{
+		str1 = haystack; /*values*/
+		str2 = needle;
+
+		/*Star WHILE*/
+		while (*haystack != '\0' && *str2 != '\0' && *haystack == *str2
+		{
+			haystack++;
+			str2++;
+		}
+		if (*str2 == '\0')
+			return (str1);
+		haystack = str1 + 1;
+	}
+	return ('\0');
 }
