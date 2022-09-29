c504c4a415cd96849bd31935190c49d5ed3f792b
henok <teklemariammossie1@gmail.com>
2022-09-29 11:01:40 -0700

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -12,6 +12,8 @@ int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
-	else
-		return (x * _pow_recursion(x < y));
+	if (y == 0)
+		return (x);
+	y--;
+		return (x * _pow_recursion(x, y));
 }
