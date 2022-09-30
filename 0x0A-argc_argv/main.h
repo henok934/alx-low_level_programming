ba677c8671ea5b9b7f48fbb8cee5a63eeefeac9b
henok <teklemariammossie1@gmail.com>
2022-09-30 12:21:32 -0700

    main.h

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/** 
+* This header file contains all the prototypes required for the
+* 0x0A-argc_argv project.
+*
+* @author: Gizachew Bayness
+*
+* Date crated September 30, 2022
+*
+/*
+
+int _putchar(char c);
+
+#endif
