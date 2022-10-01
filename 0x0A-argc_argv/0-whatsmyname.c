62a8d42b1e18c68c5f4925be2dbc1c1a331a3d45
henok <teklemariammossie1@gmail.com>
2022-10-01 05:37:14 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,20 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/**
- * main - functon must used for one ouput.
- *
- * @argc: returns number of character.
- * @argv: return the value of the character'
- *
- *return: returns the program.
- *
- */
-
-void main(int argc, char *argv[])
+void main(int argc, char *argv[]
 {
-	(void)argc;
-	(void)argv;
-	printf("./mynameis\n");
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+
+	priintf("./mynameis\n", i, argv[i]);
 	return (0);
+}
 }
