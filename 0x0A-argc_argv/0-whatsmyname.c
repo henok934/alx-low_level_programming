e39f2df58e8c9aa336ffd03f80f18a2e11a297c9
henok <teklemariammossie1@gmail.com>
2022-10-01 13:47:07 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
 
 /**
-* main - functon returns the value of argc and argv.
+* main - functon returns the value of argc and argv
 *
-*@argc: returns the value of the argc.
-*of the array argv
-*@argv: returns only for in array value.
+* @argc: returns the value of the argc
+* of the array argv
+* @argv: returns only for in array value
 *
-*return: the value return.
+* return: the value return
 *
 */
 
-int main(int argc, char **argv)
+void main(int argc, char **argv)
 {
 	if (argc)
 		printf("%s\n", *argv);
-	return (0);
+	return;
 }
