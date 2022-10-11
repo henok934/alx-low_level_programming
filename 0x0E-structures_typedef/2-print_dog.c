80073e9882d4daef04e2db3e5538b07becde8821
henok <teklemariammossie1@gmail.com>
2022-10-11 12:38:42 -0700

    2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - manipulate the function
+ * @d: variable
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+printf("name: %s\nage: %f\nowner %s\n", d->name, d->age, d->owner);
+}
+
