b9110954964426218a86370b1809713950d93b50
teklemariam mossie <teklemariammossie1@gmail.com>
2022-10-14 04:57:32 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,11 +14,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(list, n);
 
-	if (separator == NULL)
-	{
-		return;
-	}
-
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(list, int));
