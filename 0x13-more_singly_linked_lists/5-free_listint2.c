de0a17c0184a1c8499d04d1cb5cf8cb46d3283d2
teklemariam mossie <teklemariammossie1@gmail.com>
2022-10-26 04:44:34 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list_t linked list
+ * @head: A pointer to the head of the list.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *next_node;
+	
+	if (head == NULL)
+		return;
+	while (*head)
+	{
+		next_node = (*head)->next;
+		free(*head);
+		*head = next_node;
+	}
+	*head = next_node;
+}
