210bca6ac6c37bb98c346ca9cef0c9a9f354b624
henok <teklemariammossie1@gmail.com>
2022-12-09 03:45:54 -0800

    new commit

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,34 +1,38 @@
 #include "lists.h"
-#include <stdlib.h>
+
 /**
- * add_dnodeint_end - add a node at the end of a double linked list
- * @head: header of double linked list
- * @n: number to set in a new node
- * Return: address of a new element
+ * add_dnodeint_end - Adds a new element at the end of the list
+ *
+ * @head: A pointer that points to a pointer which inturn points to
+ * the head of the list.
+ * @n: The element to be added
+ *
+ * Return: The address of the new element, or NULL if it failed
+ *
  */
+
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new;
-	dlistint_t *tempo;
+	dlistint_t *temp, *new_node = malloc(sizeof(dlistint_t));
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
+	if (new_node == NULL)
 		return (NULL);
-	new->n = n;
-	new->prev = NULL;
-	if (*head == NULL)
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (!(*head))
 	{
-		new->prev = NULL;
-		*head = new;
+		new_node->prev = NULL;
+		*head = new_node;
+		return (*head);
 	}
-	else
-	{
-		tempo = *head;
-		while (tempo->next != NULL)
-			tempo->next = tempo;
-		tempo->next = new;
-		new->prev = tempo;
-		*head = new;
-	}
-	return (new);
+
+	temp = *head;
+	while (temp->next != NULL)
+		temp = temp->next;
+	temp->next = new_node;
+	new_node->prev = temp;
+
+	return (new_node);
 }
