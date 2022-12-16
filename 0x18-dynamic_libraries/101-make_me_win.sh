e13b4c8657bdc7cca3fc408173f4670d9c0a5db2
henok <teklemariammossie1@gmail.com>
2022-12-16 08:51:12 -0800

    new commit

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.githubusercontent.com/root@96701e073f37/alx-low_level_programming/master/0x18-dynamic_libraries/libtest.so
-export LD_PRELOAD=/tmp/libtest.so
+gcc -c test.c -fpic
+gcc -shared -o libtest.so test.o
